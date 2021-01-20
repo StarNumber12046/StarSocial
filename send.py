5167154e4796869d2c1924e8fdec4ca0e37cf253
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-01-20 14:42:06 +0100

    lel

--- send.py
+++ send.py
@@ -1,12 +1,16 @@
 from os import system as s
-s('pip install pymongo')
 
+s('pip install pymongo')
 s('pip install termcolor')
 s('pip install dnspython')
+
 import pymongo
 from termcolor import colored
 
 from datetime import datetime
+
+s('git pull origin master')
+
 
 
 url = 'mongodb+srv://NoOne:Something@cluster0.dhplg.mongodb.net/Cluster0?retryWrites=true&w=majority'
@@ -16,18 +20,18 @@ date = datetime.now().strftime('%x')
 cluster = pymongo.MongoClient('mongodb+srv://NoOne:Something@cluster0.dhplg.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = cluster['socialmedia'] ['messaging']
 all = db.find()
+
+time = datetime.now().strftime('%X')
 for messages in all:
     try:
         if date != messages['date']:
             print(f"""{messages['date']}""", 'red')
         else:
-            print(colored("From: ", "green"), messages['id'])
-            print(colored("Message: ", 'green'), messages['message'])
+            print(colored("From: ", "yellow"), messages['id'])
+            print(colored("Message: ", 'blue'), messages['message'])
             print('--------------------------------------------')
     except:
         pass
-
-time = datetime.now().strftime('%X')
 
 
 
@@ -36,3 +40,5 @@ message = input('Insert your message: ')
 
 msg = {"id": person, "message": message, "date": date, "time": time}
 db.insert_one(msg)
+
+
